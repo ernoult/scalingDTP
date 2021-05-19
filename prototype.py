5755bdca8569913abdc24bb4c635218ad2f3925f
Ernoult <ernoult.m@gmail.com>
2021-05-19 22:29:24 +0200

    initializing the branch with the simplified code

--- prototype.py
+++ prototype.py
@@ -7,9 +7,6 @@ from models import *
 
 parser = argparse.ArgumentParser(description='Testing idea of Yoshua')
 
- 
-parser.add_argument('--in_channels', type=int, default=1, help='input channels (default: 1)')   
-parser.add_argument('--out_channels', type=int, default=128, help='output channels (default: 128)')   
 parser.add_argument('--epochs', type=int, default=15, help='number of epochs to train feedback weights(default: 15)') 
 parser.add_argument('--iter', nargs = '+', type=int, default=[5, 10], help='number of learning iterating of feedback weights layer-wise per batch (default: [5, 10])')
 parser.add_argument('--batch-size', type=int, default=128, help='batch dimension (default: 128)')   
@@ -17,21 +14,13 @@ parser.add_argument('--device-label', type=int, default=0, help='device (default
 parser.add_argument('--lr_f', type=float, default=0.05, help='learning rate (default: 0.05)')   
 parser.add_argument('--lr_b', nargs = '+', type=float, default=[0.05, 0.05], help='learning rates for the feedback weights (default: [0.05, 0.05])')
 parser.add_argument('--beta', type=float, default=0.1, help='nudging parameter (default: 0.1)')   
-parser.add_argument('--sym', default=False, action='store_true',help='sets symmetric weight initialization (default: False)')
-parser.add_argument('--jacobian', default=False, action='store_true',help='compute jacobians (default: False)')
 parser.add_argument('--C', nargs = '+', type=int, default=[32, 64], help='tab of channels (default: [32, 64])')
-parser.add_argument('--sigmapi', default=False, action = 'store_true', help='use of sigma-pi G functions (default: False)')
-parser.add_argument('--mlp', default=False, action = 'store_true', help='use of MLP G functions (default: False)')
 parser.add_argument('--noise', nargs = '+', type=float, default=[0.05, 0.5], help='tab of noise amplitude (default: [0.05, 0.5])')
-parser.add_argument('--action', nargs = '+', type=str, default=['test'], help='action and subaction to take (default: [test])')
-parser.add_argument('--alg', nargs = '+', type=int, default=[1, 2], help='algorithm used to train the feedback weights layer-wise (default: [1, 2])')
 parser.add_argument('--activation', type=str, default='elu', help='activation function in conv layers (default: elu)')
 parser.add_argument('--path', type=str, default= None, help='Path directory for the results (default: None)')
 parser.add_argument('--last-trial', default=False, action='store_true',help='specifies if the current trial is the last one (default: False)')
 parser.add_argument('--seed', type=int, default=None, help='seed selected (default: None)')
-parser.add_argument('--jac', default=False, action='store_true',help='compute jacobian distance/angle instead of weight distance/angle (default: False)')
 parser.add_argument('--scheduler', default=False, action='store_true',help='use of a learning rate scheduler for the forward weights (default: False)')      
-parser.add_argument('--dataset', type=str, default= 'mnist', help='Dataset (default: mnist)')
 parser.add_argument('--wdecay', type=float, default=None, help='Weight decay (default: None)')   
 
 args = parser.parse_args()  
@@ -65,12 +54,7 @@ if __name__ == '__main__':
         createHyperparameterfile(BASE_PATH, command_line, seed, args)
 
 
-    if args.dataset == 'mnist':
-        net = smallNet(args)
-
-    elif args.dataset == 'cifar10':
-        net = VGG(args)
-    
+    net = VGG(args)
     net = net.to(device)
     print(net)    
 
@@ -78,25 +62,21 @@ if __name__ == '__main__':
         criterion = torch.nn.CrossEntropyLoss(reduction='none')
         optimizers = createOptimizers(net, args, forward = True)
 
-        #************************************************************************************************#
         if args.scheduler:
-            #scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizers[0], milestones=[30,60], gamma=0.1)
             scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizers[0], 85, eta_min=1e-5)
             print('We are using a learning rate scheduler!')
-        #************************************************************************************************#
 
                 
         train_acc = []
         test_acc = []
 
-        #**STANDARD GENERAL TRAINING WITH FORWARD AND FEEDBACK WEIGHTS**#
         for epoch in range(args.epochs):
-            net.train()
+            
             train_loss = 0
             correct = 0
             total = 0
             for batch_idx, (data, target) in enumerate(train_loader):
-
+                net.train()
                 data, target = data.to(device), target.to(device)
                 pred, loss = train_batch(args, net, data, optimizers, target, criterion)
 
@@ -118,96 +98,10 @@ if __name__ == '__main__':
                 pickle.dump(results, outfile)
                 outfile.close()
 
-            #*******************#
             if args.scheduler:
                 scheduler.step()
-            #*******************#
 
+            if (train_acc[-1] < 30): exit()                 
 
-            if (args.dataset == 'mnist') and (train_acc[-1] < 80): exit()
-            elif (args.dataset == 'cifar10') and (train_acc[-1] < 30): exit()                 
-
-    elif args.action[0] == 'test':
-        criterion = torch.nn.CrossEntropyLoss(reduction='none')
-           
-        optimizers = createOptimizers(net, args)
-        
-        weight_tab = []
-        loss_tab = []
-
-        weight_tab = {'dist_weight' : [], 'angle_weight' : []}        
-
-        for batch_iter in range(args.epochs):
-            print('\n Batch iteration {}'.format(batch_iter + 1))
-            _, (data, _) = next(enumerate(train_loader))             
-            data = data.to(device)
-
-            weight_tab = train_batch(args, net, data, optimizers, tabs = weight_tab, batch_iter = batch_iter)
-               
-
-            if args.path is not None:
-                results = {'weight_tab' : weight_tab}
-                outfile = open(os.path.join(BASE_PATH, 'results'), 'wb')
-                pickle.dump(results, outfile)
-                outfile.close() 
-
-        
-    elif args.action[0] == 'debug':
-
-        criterion = torch.nn.CrossEntropyLoss(reduction='none')
-        optimizer_f, optimizer_b = createOptimizers(net, args, forward = True)
-
-        _, (data, target) = next(enumerate(train_loader))
-        data, target = data.to(device), target.to(device)
-
-        #Testing forward pass
-        '''
-        y = net(data)
-        print('Forward pass works!')        
-        '''
-        
-        #Testing feedback weights training
-        '''        
-        y = net.layers[0](data).detach() 
-        for id_layer in range(len(net.layers) - 1):                     
-            if id_layer < len(net.layers) - 2:
-                print('Training feedback convpool layer ' + str(id_layer + 1))
-            else:
-                print('Training feedback FC layer')  
-
-            net.layers[id_layer + 1].weight_b_train(y, optimizer_b)
-            
-            #go to the next layer
-            y = net.layers[id_layer + 1](y).detach()
-
-        optimizer_b.zero_grad()
-        print('Feedback weights training works!')        
-        '''
-
-        #Testing forward weights training
-        '''
-        y, r = net(data, ind_layer = len(net.layers))
-        
-        L = criterion(y.float(), target).squeeze()
-        init_grads = torch.tensor([1 for i in range(y.size(0))], dtype=torch.float, device=device, requires_grad=True) 
-        grads = torch.autograd.grad(L, y, grad_outputs=init_grads, create_graph = True)
-        delta = -args.beta*grads[0]
-        
-        t = y + delta
-
-        for id_layer in range(len(net.layers)):        
-            print('Step {}'.format(id_layer + 1))
-            loss_f = net.layers[-1 - id_layer].weight_f_train(y, t, optimizer_f)
-    
-            #compute previous targets         
-            if (id_layer < len(net.layers) - 1):
-                delta = net.layers[-1 - id_layer].propagateError(r, t)
-                y, r = net(data, ind_layer = len(net.layers) - 1 - id_layer)
-                t = (y + delta).detach()
-
-            if id_layer == 0:
-                loss = loss_f
-        print('Forward weights training works!') 
-        '''
-              
+ 
 
--- tools.py
+++ tools.py
@@ -157,11 +157,9 @@ def createHyperparameterfile(BASE_PATH, command_line, seed, args):
         "- number of feedback optim steps per batch: {}".format(args.iter) + "\n",
         "- activation function: {}".format(args.activation) + "\n",
         "- noise: {}".format(args.noise) + "\n",
-        "- algorithm used to train the feedback weights: {}".format(args.alg) + "\n",
         "- learning rate for forward weights: {}".format(args.lr_f) + "\n",
         "- learning rate for feedback weights: {}".format(args.lr_b) + "\n",
         "- batch size: {}".format(args.batch_size) + "\n",
-        "- symmetric weight initialization: {}".format(args.sym) + "\n",
         "- number of epochs: {}".format(args.epochs) + "\n",
         "- seed: {}".format(seed) + "\n", 
         "\n To reproduce this simulation, type in the terminal:\n",
@@ -179,97 +177,47 @@ def createHyperparameterfile(BASE_PATH, command_line, seed, args):
     st = os.stat(script_name)
     os.chmod(script_name, st.st_mode | stat.S_IEXEC)    
  
-'''
-def train(epoch):
-    print('\nEpoch: %d' % epoch)
-    net.train()
-    train_loss = 0
-    correct = 0
-    total = 0
-    for batch_idx, (inputs, targets) in enumerate(train_loader):
-        inputs, targets = inputs.to(device), targets.to(device)
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-
-        train_loss += loss.item()
-        _, predicted = outputs.max(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
-
-        progress_bar(batch_idx, len(train_loader), 'Loss: %.3f | Train Acc: %.3f%% (%d/%d)'
-                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
-'''
 
 def train_batch(args, net, data, optimizers, target = None, criterion = None, **kwargs):
 
-    if args.action[0] == 'train':
-        optimizer_f, optimizer_b = optimizers
+    optimizer_f, optimizer_b = optimizers
 
 
-        #****FEEDBACK WEIGHTS****#          
-        y = net.layers[0](data).detach() 
-        for id_layer in range(len(net.layers) - 1):                     
-            net.layers[id_layer + 1].weight_b_train(y, optimizer_b)
-            y = net.layers[id_layer + 1](y).detach()
+    #****FEEDBACK WEIGHTS TRAINING****#          
+    y = net.layers[0](data).detach() 
+    for id_layer in range(len(net.layers) - 1):                     
+        net.layers[id_layer + 1].weight_b_train(y, optimizer_b)
+        y = net.layers[id_layer + 1](y).detach()
 
-        pred = torch.exp(net.logsoft(y)) 
+    pred = torch.exp(net.logsoft(y)) 
 
-        #*********FORWARD WEIGHTS********#
-        y, r = net(data, ind_layer = len(net.layers))
 
-        L = criterion(y.float(), target).squeeze()
-        init_grads = torch.tensor([1 for i in range(y.size(0))], dtype=torch.float, device=y.device, requires_grad=True) 
-        grads = torch.autograd.grad(L, y, grad_outputs=init_grads, create_graph = True)
-        delta = -args.beta*grads[0]
 
-        t = y + delta
+    #*********FORWARD WEIGHTS TRAINING********#
+    y, r = net(data, ind_layer = len(net.layers))
 
-        for id_layer in range(len(net.layers)):        
-            
-            loss_f = net.layers[-1 - id_layer].weight_f_train(y, t, optimizer_f)
+    L = criterion(y.float(), target).squeeze()
+    init_grads = torch.tensor([1 for i in range(y.size(0))], dtype=torch.float, device=y.device, requires_grad=True) 
+    grads = torch.autograd.grad(L, y, grad_outputs=init_grads, create_graph = True)
+    delta = -args.beta*grads[0]
 
-            #compute previous targets         
-            if (id_layer < len(net.layers) - 1):
-                delta = net.layers[-1 - id_layer].propagateError(r, t)
-                y, r = net(data, ind_layer = len(net.layers) - 1 - id_layer)
-                t = (y + delta).detach()
-            
-            if id_layer == 0:
-                loss = loss_f
+    t = y + delta
+
+    for id_layer in range(len(net.layers)):        
         
+        loss_f = net.layers[-1 - id_layer].weight_f_train(y, t, optimizer_f)
+
+        #compute previous targets         
+        if (id_layer < len(net.layers) - 1):
+            delta = net.layers[-1 - id_layer].propagateError(r, t)
+            y, r = net(data, ind_layer = len(net.layers) - 1 - id_layer)
+            t = (y + delta).detach()
         
-        return pred, loss
+        if id_layer == 0:
+            loss = loss_f
+    
+    return pred, loss
 
-    elif args.action[0] == 'test':
-        optimizer_b = optimizers
-        weight_tab = kwargs['tabs']
-        batch_iter = kwargs['batch_iter']
-        y = net.layers[0](data).detach()
-        
-        for id_layer in range(len(net.layers) - 1):  
-            if (len(args.action) == 1) or id_layer == int(args.action[1]):
-                loss_b = net.layers[id_layer + 1].weight_b_train(y, optimizer_b)
-
-                if (batch_iter + 1) % 10  == 0: 
-                    dist_weight, angle_weight = net.layers[id_layer + 1].compute_dist_angle(y)
-                    
-                    if id_layer < len(net.layers) - 2:
-                        print('Conv layer ' + str(id_layer + 1) + ': ')
-                    else:
-                        print('FC layer:')  
-
-                    print('Distance between weights: {:.2f}'.format(dist_weight))
-                    print('Weight angle: {:.2f} deg'.format(angle_weight))  
-                    weight_tab['dist_weight'].append(dist_weight)
-                    weight_tab['angle_weight'].append(angle_weight)       
-
-            #go to the next layer
-            y = net.layers[id_layer + 1](y).detach()
-
-        return weight_tab
 
 
 
@@ -302,8 +250,6 @@ def createOptimizers(net, args, forward = False):
     
     else:
         return optimizer_b
-
-
 
 
 def test(net, test_loader, device):
@@ -339,63 +285,24 @@ def copy(y, ind_y):
 
 def createDataset(args):
 
-    if args.dataset == 'mnist':
-        class ReshapeTransform:
-            def __init__(self, new_size):
-                self.new_size = new_size
+    transform_train = torchvision.transforms.Compose([torchvision.transforms.RandomHorizontalFlip(0.5),
+                                                      torchvision.transforms.RandomCrop(size=[32,32], padding=4, padding_mode='edge'),
+                                                      torchvision.transforms.ToTensor(), 
+                                                      torchvision.transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), 
+                                                                                       std=(3*0.2023, 3*0.1994, 3*0.2010)) ])    
 
-            def __call__(self, img):
-                return torch.reshape(img, self.new_size)
+    transform_test = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), 
+                                                     torchvision.transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), 
+                                                                                      std=(3*0.2023, 3*0.1994, 3*0.2010)) ]) 
 
-        class ReshapeTransformTarget:
-            def __init__(self, number_classes):
-                self.number_classes = number_classes
-            
-            def __call__(self, target):
-                target=torch.tensor(target).unsqueeze(0).unsqueeze(1)
-                target_onehot = torch.zeros((1,self.number_classes))      
-                return target_onehot.scatter_(1, target, 1).squeeze(0)
+    cifar10_train_dset = torchvision.datasets.CIFAR10('./cifar10_pytorch', train=True, transform=transform_train, download=True)
+    cifar10_test_dset = torchvision.datasets.CIFAR10('./cifar10_pytorch', train=False, transform=transform_test, download=True)
+   
+    val_index = np.random.randint(10)
+    val_samples = list(range( 5000 * val_index, 5000 * (val_index + 1) ))
 
-
-        transforms=[torchvision.transforms.ToTensor()]
-        train_loader = torch.utils.data.DataLoader(
-        torchvision.datasets.MNIST(root='./data', train=True, download=True,
-                                 transform=torchvision.transforms.Compose(transforms)
-                                ),
-        batch_size = args.batch_size, shuffle=True)
-
-
-        test_loader = torch.utils.data.DataLoader(
-        torchvision.datasets.MNIST(root='./data', train=False, download=True,
-                                 transform=torchvision.transforms.Compose(transforms)
-                                ),
-        batch_size = args.batch_size, shuffle=True)
-
-    elif args.dataset == 'cifar10':
-        '''
-        transform_train = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), 
-                                                        torchvision.transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), 
-                                                                                               std=(3*0.2023, 3*0.1994, 3*0.2010)) ])
-        '''
-
-        transform_train = torchvision.transforms.Compose([torchvision.transforms.RandomHorizontalFlip(0.5),
-                                                          torchvision.transforms.RandomCrop(size=[32,32], padding=4, padding_mode='edge'),
-                                                          torchvision.transforms.ToTensor(), 
-                                                          torchvision.transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), 
-                                                                                           std=(3*0.2023, 3*0.1994, 3*0.2010)) ])    
-
-        transform_test = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), 
-                                                         torchvision.transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), 
-                                                                                          std=(3*0.2023, 3*0.1994, 3*0.2010)) ]) 
-
-        cifar10_train_dset = torchvision.datasets.CIFAR10('./cifar10_pytorch', train=True, transform=transform_train, download=True)
-        cifar10_test_dset = torchvision.datasets.CIFAR10('./cifar10_pytorch', train=False, transform=transform_test, download=True)
-       
-        val_index = np.random.randint(10)
-        val_samples = list(range( 5000 * val_index, 5000 * (val_index + 1) ))
-
-        train_loader = torch.utils.data.DataLoader(cifar10_train_dset, batch_size=args.batch_size, shuffle=True, num_workers=1)
-        test_loader = torch.utils.data.DataLoader(cifar10_test_dset, batch_size=200, shuffle=False, num_workers=1)
+    train_loader = torch.utils.data.DataLoader(cifar10_train_dset, batch_size=args.batch_size, shuffle=True, num_workers=1)
+    test_loader = torch.utils.data.DataLoader(cifar10_test_dset, batch_size=200, shuffle=False, num_workers=1)
 
     return train_loader, test_loader
 
