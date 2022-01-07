7f1d230938c31a76291432e630691e435d7f76a5
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-01-07 15:14:08 -0500

    Fixing python 3.7 issues
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- target_prop/backward_layers.py
+++ target_prop/backward_layers.py
@@ -4,12 +4,14 @@ from functools import singledispatch
 from typing import (
     Any,
     OrderedDict,
-    Protocol,
     Tuple,
     TypeVar,
     Union,
-    runtime_checkable,
 )
+try:
+    from typing import Protocol runtime_checkable
+except ImportError:
+    from typing_extensions import Protocol, runtime_checkable
 
 from torch import Tensor, nn
 
