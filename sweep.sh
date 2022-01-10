a9a933d5a7ae0642b4156bee749a0680eeed73d8
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-01-10 11:37:12 -0500

    Add sweep script, parametrize run/sweep.sh
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- sweep.sh
+++ sweep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --array=0-5%2
+#SBATCH --array=0-20%5
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:1
 #SBATCH --mem=10GB
@@ -8,4 +8,4 @@ conda activate target_prop
 
 cd ~/target_prop/scalingDTP
 
-python main_pl.py run "$@" --data_dir $SLURM_TMPDIR --seed $SLURM_ARRAY_TASK_ID
+python main_pl.py sweep "$@" --data_dir $SLURM_TMPDIR --seed $SLURM_ARRAY_TASK_ID
