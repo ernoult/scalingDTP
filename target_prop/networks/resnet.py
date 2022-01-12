45aa7d32c256f14d48cf62cc0801ebeab7785692
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2022-01-11 23:41:36 -0500

    Minor cleanup

--- target_prop/networks/resnet.py
+++ target_prop/networks/resnet.py
@@ -119,42 +119,6 @@ def invert_residual(module: ResidualBlock) -> InvertedResidualBlock:
     return backward
 
 
-# def ResidualBlock(in_planes, planes, stride=1, use_batchnorm=False):
-#     """
-#     Basic residual block with optional BatchNorm.
-#     Adapted from PyTorch ResNet: https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py
-#     """
-
-#     expansion = 1
-
-
-#     def __init__(self, in_planes, planes, stride=1, use_batchnorm=False):
-#         super(ResidualBlock, self).__init__()
-#         self.use_batchnorm = use_batchnorm
-#         self.conv1 = nn.Conv2d(
-#             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
-#         )
-#         self.bn1 = nn.BatchNorm2d(planes) if self.use_batchnorm else nn.Identity()
-#         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-#         self.bn2 = nn.BatchNorm2d(planes) if self.use_batchnorm else nn.Identity()
-
-#         self.shortcut = nn.Sequential()
-#         if stride != 1 or in_planes != self.expansion * planes:
-#             self.shortcut = nn.Sequential(
-#                 nn.Conv2d(
-#                     in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False
-#                 ),
-#                 nn.BatchNorm2d(self.expansion * planes) if self.use_batchnorm else nn.Identity(),
-#             )
-
-#     def forward(self, x):
-#         out = F.relu(self.bn1(self.conv1(x)))
-#         out = self.bn2(self.conv2(out))
-#         out += self.shortcut(x)
-#         out = F.relu(out)
-#         return out
-
-
 def make_layer(block, planes, num_blocks, stride, in_planes, use_batchnorm):
     strides = [stride] + [1] * (num_blocks - 1)
     layers = []
