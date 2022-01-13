b86072635e0f679fd75cd406112b538fad832e47
SpinnSean <sean.c.spinney@gmail.com>
2022-01-13 17:51:22 -0500

    Alternate formulation of lenet

--- target_prop/networks/lenet.py
+++ target_prop/networks/lenet.py
@@ -28,41 +28,43 @@ def lenet(in_channels, n_classes, hparams):
 
     # NOTE: Can use [0:] and [1:] below because zip will stop when the shortest
     # iterable is exhausted. This gives us the right number of blocks.
-    for i, (in_channels, out_channels) in enumerate(zip(channels[0:], channels[1:])):
+    for i, (in_channels, out_channels) in enumerate(zip(channels[0:-1], channels[1:-1])):
 
         if i == len(hparams.channels) - 1:
-            layers["fc"] = nn.Sequential(
-                OrderedDict(
-                    reshape=Reshape(target_shape=(-1,)),
-                    linear1=nn.LazyLinear(out_features=512, bias=True),
-                    rho=activation(),
-                    linear2=nn.Linear(in_features=512, out_features=n_classes, bias=True)
-                )
+
+
+        block = nn.Sequential(
+            OrderedDict(
+                conv=nn.Conv2d(
+                    in_channels,
+                    out_channels,
+                    kernel_size=5,
+                    stride=1,
+                    padding=2, # in Meuleman code padding=2
+                ),
+                rho=activation(),
+                # NOTE: Even though `return_indices` is `False` here, we're actually passing
+                # the indices to the backward net for this layer through a "magic bridge".
+                # We use `return_indices=False` here just so the layer doesn't also return
+                # the indices in its forward pass.
+                pool=MaxPool2d(kernel_size=3, stride=2, return_indices=False),
+                # NOTE: Would be nice to use AvgPool, seems more "plausible" and less hacky.
+                # pool=nn.AvgPool2d(kernel_size=2),
             )
-
-        else:
-
-            block = nn.Sequential(
-                OrderedDict(
-                    conv=nn.Conv2d(
-                        in_channels,
-                        out_channels,
-                        kernel_size=5,
-                        stride=1,
-                        padding=2, # in Meuleman code padding=2
-                    ),
-                    rho=activation(),
-                    # NOTE: Even though `return_indices` is `False` here, we're actually passing
-                    # the indices to the backward net for this layer through a "magic bridge".
-                    # We use `return_indices=False` here just so the layer doesn't also return
-                    # the indices in its forward pass.
-                    pool=MaxPool2d(kernel_size=3, stride=2, return_indices=False),
-                    # NOTE: Would be nice to use AvgPool, seems more "plausible" and less hacky.
-                    # pool=nn.AvgPool2d(kernel_size=2),
-                )
-            )
-            layers[f"conv_{i}"] = block
-
+        )
+        layers[f"conv_{i}"] = block
+    layers["fc1"] = nn.Sequential(
+        OrderedDict(
+            reshape=Reshape(target_shape=(-1,)),
+            linear1= nn.LazyLinear(out_features=hparams.channels[-1], bias=True),
+            rho=activation(),
+        )
+    )
+    layers["fc2"] = nn.Sequential(
+        OrderedDict(
+            linear2= nn.Linear(in_features=512, out_features=n_classes, bias=True)
+        )
+    )
     # layers["reshape"] = Reshape(target_shape=(-1,))
     # # # NOTE: Using LazyLinear so we don't have to know the hidden size in advance
     # layers["fc"] = nn.LazyLinear(out_features=self.n_classes, bias=True)
