66a9155f2083f7d0c91cbe901dd7086578c1d4fc
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-01-19 14:35:59 -0500

    Fix tiny bug (missing import)
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- target_prop/datasets/__init__.py
+++ target_prop/datasets/__init__.py
@@ -1,2 +1,6 @@
-from .cifar10_datamodule import CIFAR10DataModule, cifar10_normalization
+from .cifar10_datamodule import (
+    CIFAR10DataModule,
+    cifar10_normalization,
+    cifar10_3xstd_normalization,
+)
 from .imagenet32_datamodule import ImageNet32DataModule, imagenet32_normalization
