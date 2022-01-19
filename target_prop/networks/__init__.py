975d669e79b68220170ff2bb7dc72cbb1104ddf5
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-01-19 13:51:01 -0500

    Add Network protocol (typing improvement only)
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- target_prop/networks/__init__.py
+++ target_prop/networks/__init__.py
@@ -1,6 +1,6 @@
 from .resnet import ResNet18Hparams, ResNet34Hparams, resnet, ResNet, ResNet18, ResNet34
 from .simple_vgg import SimpleVGGHparams, simple_vgg, SimpleVGG
 from .lenet import LeNet
+from .network import Network
 from typing import Union
 
-Network = Union[ResNet18, ResNet34, SimpleVGG, LeNet]
