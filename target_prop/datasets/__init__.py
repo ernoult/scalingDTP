3b5ee9a5dda22fa9f1b7c6630e437473e3432135
SpinnSean <sean.c.spinney@gmail.com>
2022-01-20 17:04:19 -0500

    Added mnist datamodule.

--- target_prop/datasets/__init__.py
+++ target_prop/datasets/__init__.py
@@ -1,2 +1,3 @@
+from .mnist_datamodule import MNISTDataModule, mnist_normalization
 from .cifar10_datamodule import CIFAR10DataModule, cifar10_normalization, cifar10_3xstd_normalization
 from .imagenet32_datamodule import ImageNet32DataModule, imagenet32_normalization
