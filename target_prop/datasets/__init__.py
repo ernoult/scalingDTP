6b2f79cd1a55e517277155ee3bc8df91c80268c8
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2022-01-20 02:04:49 -0500

    Trying out 3x std normalization

--- target_prop/datasets/__init__.py
+++ target_prop/datasets/__init__.py
@@ -1,6 +1,2 @@
-from .cifar10_datamodule import (
-    CIFAR10DataModule,
-    cifar10_normalization,
-    cifar10_3xstd_normalization,
-)
-from .imagenet32_datamodule import ImageNet32DataModule, imagenet32_normalization
+from .cifar10_datamodule import CIFAR10DataModule, cifar10_normalization, cifar10_3xstd_normalization
+from .imagenet32_datamodule import ImageNet32DataModule, imagenet32_normalization, imagenet32_3xstd_normalization
