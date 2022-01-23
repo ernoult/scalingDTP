8763fbaef0c9379608de83232e6d8f69790b2e4a
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2022-01-22 21:37:59 -0500

    Fix CIFAR10 std values

--- target_prop/datasets/cifar10_datamodule.py
+++ target_prop/datasets/cifar10_datamodule.py
@@ -11,11 +11,10 @@ from torchvision.datasets import CIFAR10
 
 def cifar10_normalization():
     # From PL bolts
-    # normalize = transforms.Normalize(
-    #     mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
-    #     std=[x / 255.0 for x in [63.0, 62.1, 66.7]],
-    # )
-    normalize = transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
+    normalize = transforms.Normalize(
+        mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+        std=[x / 255.0 for x in [63.0, 62.1, 66.7]],
+    )
     return normalize
 
 
