d046852549a063587670328000297b94fff5d176
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2022-01-26 18:51:06 -0500

    Cleanup imports

--- target_prop/networks/lenet_test.py
+++ target_prop/networks/lenet_test.py
@@ -29,37 +29,6 @@ from target_prop.models.dtp import FeedbackOptimizerConfig, ForwardOptimizerConf
 from target_prop.networks import LeNet
 from target_prop.utils import is_trainable
 
-# import logging
-# from collections import OrderedDict
-# from dataclasses import dataclass
-# from typing import ClassVar, Iterable, List, Optional, Tuple, Type
-
-# import pytest
-# import torch
-# from pytorch_lightning import Trainer
-# from pytorch_lightning.utilities.seed import seed_everything
-# from simple_parsing.helpers import choice, list_field
-# from simple_parsing.helpers.hparams import log_uniform, uniform
-# from simple_parsing.helpers.hparams.hyperparameters import HyperParameters
-# from target_prop._weight_operations import init_symetric_weights
-# from target_prop.backward_layers import mark_as_invertible
-# from target_prop.config import Config
-# from target_prop.layers import Reshape, forward_all, invert
-# from target_prop.legacy import (
-#     VGG,
-#     createDataset,
-#     createOptimizers,
-#     train_backward,
-#     train_batch,
-#     train_forward,
-# )
-# from target_prop.metrics import compute_dist_angle
-# from target_prop.models import DTP
-# from target_prop.networks.simple_vgg import SimpleVGG
-# from target_prop.utils import is_trainable, named_trainable_parameters
-# from torch import Tensor, nn
-# from torch.nn import functional as F
-
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
