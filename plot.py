7d73e5bc339ab7a78b51ca89ea58445cce633ed8
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2022-01-27 04:37:07 -0500

    Add quick plotting script + angle data

---
+++ plot.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# Read data
+data_path = "./data/angle_data.csv"
+save_path = "./data/angle_plot.png"
+df = pd.read_csv(data_path)
+
+# Save figure
+sns.set_theme(style="darkgrid")  # "whitgrid" also looks nice
+fig = sns.catplot(
+    data=df,
+    x="init_scheme",
+    y="angle",
+    hue="param",
+    kind="bar",
+    height=5,
+    aspect=1.5,
+    alpha=0.9,
+    legend_out=False,
+    errwidth=2,
+    capsize=0.05,
+)
+fig.set_axis_labels("", "angle")
+fig.legend.set_title("params")
+plt.savefig(save_path, bbox_inches="tight")
