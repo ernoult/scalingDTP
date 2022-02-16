925fb3713b6700c716522c5cf3065834d41d6059
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-02-16 17:05:34 -0500

    Move stuff inside main function in main_legacy
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- main_legacy.py
+++ main_legacy.py
@@ -9,86 +9,88 @@ from tqdm import tqdm
 
 from target_prop.legacy import *
 
-parser = argparse.ArgumentParser(description="Testing idea of Yoshua")
 
-parser.add_argument(
-    "--epochs", type=int, default=15, help="number of epochs to train feedback weights(default: 15)"
-)
-parser.add_argument(
-    "--iter",
-    nargs="+",
-    type=int,
-    default=[5, 10],
-    help="number of learning iterating of feedback weights layer-wise per batch (default: [5, 10])",
-)
-parser.add_argument("--batch-size", type=int, default=128, help="batch dimension (default: 128)")
-parser.add_argument("--device-label", type=int, default=0, help="device (default: 1)")
-parser.add_argument("--lr_f", type=float, default=0.05, help="learning rate (default: 0.05)")
-parser.add_argument(
-    "--lr_b",
-    nargs="+",
-    type=float,
-    default=[0.05, 0.05],
-    help="learning rates for the feedback weights (default: [0.05, 0.05])",
-)
-parser.add_argument("--beta", type=float, default=0.1, help="nudging parameter (default: 0.1)")
-parser.add_argument(
-    "--C", nargs="+", type=int, default=[32, 64], help="tab of channels (default: [32, 64])"
-)
-parser.add_argument(
-    "--noise",
-    nargs="+",
-    type=float,
-    default=[0.05, 0.5],
-    help="tab of noise amplitude (default: [0.05, 0.5])",
-)
-parser.add_argument(
-    "--activation",
-    type=str,
-    default="elu",
-    help="activation function in conv layers (default: elu)",
-)
-parser.add_argument(
-    "--path", type=str, default=None, help="Path directory for the results (default: None)"
-)
-parser.add_argument(
-    "--last-trial",
-    default=False,
-    action="store_true",
-    help="specifies if the current trial is the last one (default: False)",
-)
-parser.add_argument("--seed", type=int, default=None, help="seed selected (default: None)")
-parser.add_argument(
-    "--scheduler",
-    default=False,
-    action="store_true",
-    help="use of a learning rate scheduler for the forward weights (default: False)",
-)
-parser.add_argument("--wdecay", type=float, default=None, help="Weight decay (default: None)")
+def main():
+    parser = argparse.ArgumentParser(description="Testing idea of Yoshua")
 
-args = parser.parse_args()
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=15,
+        help="number of epochs to train feedback weights(default: 15)",
+    )
+    parser.add_argument(
+        "--iter",
+        nargs="+",
+        type=int,
+        default=[5, 10],
+        help="number of learning iterating of feedback weights layer-wise per batch (default: [5, 10])",
+    )
+    parser.add_argument(
+        "--batch-size", type=int, default=128, help="batch dimension (default: 128)"
+    )
+    parser.add_argument("--device-label", type=int, default=0, help="device (default: 1)")
+    parser.add_argument("--lr_f", type=float, default=0.05, help="learning rate (default: 0.05)")
+    parser.add_argument(
+        "--lr_b",
+        nargs="+",
+        type=float,
+        default=[0.05, 0.05],
+        help="learning rates for the feedback weights (default: [0.05, 0.05])",
+    )
+    parser.add_argument("--beta", type=float, default=0.1, help="nudging parameter (default: 0.1)")
+    parser.add_argument(
+        "--C", nargs="+", type=int, default=[32, 64], help="tab of channels (default: [32, 64])"
+    )
+    parser.add_argument(
+        "--noise",
+        nargs="+",
+        type=float,
+        default=[0.05, 0.5],
+        help="tab of noise amplitude (default: [0.05, 0.5])",
+    )
+    parser.add_argument(
+        "--activation",
+        type=str,
+        default="elu",
+        help="activation function in conv layers (default: elu)",
+    )
+    parser.add_argument(
+        "--path", type=str, default=None, help="Path directory for the results (default: None)"
+    )
+    parser.add_argument(
+        "--last-trial",
+        default=False,
+        action="store_true",
+        help="specifies if the current trial is the last one (default: False)",
+    )
+    parser.add_argument("--seed", type=int, default=None, help="seed selected (default: None)")
+    parser.add_argument(
+        "--scheduler",
+        default=False,
+        action="store_true",
+        help="use of a learning rate scheduler for the forward weights (default: False)",
+    )
+    parser.add_argument("--wdecay", type=float, default=None, help="Weight decay (default: None)")
 
-train_loader, test_loader = createDataset(args)
+    args = parser.parse_args()
 
+    train_loader, test_loader = createDataset(args)
 
-if args.device_label >= 0:
-    device = torch.device("cuda:" + str(args.device_label))
-else:
-    device = torch.device("cpu")
+    if args.device_label >= 0:
+        device = torch.device("cuda:" + str(args.device_label))
+    else:
+        device = torch.device("cpu")
 
+    if args.seed is not None:
+        seed = args.seed
+        torch.manual_seed(seed)
+    else:
+        g = torch.Generator(device=device)
+        seed = g.seed()
+        torch.manual_seed(seed)
 
-if args.seed is not None:
-    seed = args.seed
-    torch.manual_seed(seed)
-else:
-    g = torch.Generator(device=device)
-    seed = g.seed()
-    torch.manual_seed(seed)
-
-print("Selected seed: {}".format(seed))
-
-
-if __name__ == "__main__":
+    print("Selected seed: {}".format(seed))
 
     # Create a directory to save results and hyperparameters
     if args.path is not None:
@@ -165,3 +167,7 @@ if __name__ == "__main__":
         if train_accuracy < 0.30:
             print(f"Accuracy is terrible ({train_accuracy:.2%}), exiting early")
             break
+
+
+if __name__ == "__main__":
+    main()
