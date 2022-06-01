a4f8b646882c8704fb634fddccfadc6bc0aeb661
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-06-01 14:14:27 -0400

    Increase required python to 3.9
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     install_requires=[
         "simple-parsing",
         "torch",
